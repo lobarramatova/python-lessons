7d98d06436d7cfe13115365b0a2eea308abc94e0
lobarramatova <lobarrramatova21@gmail.com>
2026-05-07 18:15:54 +0500

    comment

--- lambda-function.py
+++ lambda-function.py
@@ -32,4 +32,18 @@ sonlar = [5, 8, -8, 0, 13]
 
 # print(sonlar2)
 
+# map(func, iterable)
 print(list(map(lambda x : x * 2, sonlar)))
+print(list(map(lambda x : x % 3, sonlar)))          
+
+
+# filter(func, iterable)
+print(list(filter(lambda x : x > 5, sonlar)))
+students = ['Elbek', 'Lobar', "G'ulomjon", "Behruz", 'Lola']
+new_list = map(lambda ism: ism.lower(), students)
+print(list(filter(lambda ism: ism.startswith("l"), new_list)))
+
+def func(x):
+    return x + 5
+
+print(list(map(func, sonlar)))
